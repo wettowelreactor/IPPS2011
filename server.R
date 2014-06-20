0287ac09c2398d9f079145a95805c92c842c751c
Camille Roussel <camille@rousselfamily.com>
2014-06-19 20:17:08 -0400

    Core functionality up. Needs layout work.

--- server.R
+++ server.R
@@ -1,18 +1,64 @@
 library(shiny)
+
 data <- read.csv(unz('./data/IPPS_FY2011.csv.zip', 'IPPS_FY2011.csv'))
 data$Average.Covered.Charges <- as.numeric(data$Average.Covered.Charges)
 data$Average.Total.Payments <- as.numeric(data$Average.Total.Payments)
+data$Average.Medicare.Payments <- as.numeric(data$Average.Medicare.Payments)
 
 shinyServer(function(input, output) {
     
-    output$hist_charge_plot <- renderPlot({
+    output$box_average_charge_plot <- renderPlot({
         
-        hist(
-             Average.Covered.Charges,
-             data = data[data$DRG.Definition == input$drg & data$Provider.State == input$state,],
-             breaks = 100,
-             xlab = "Charge Amount",
-             main = "Average Charge Amount")
+        state1.data <- data[data$DRG.Definition == input$drg & data$Provider.State == input$state1,]
+        state2.data <- data[data$DRG.Definition == input$drg & data$Provider.State == input$state2,]
+        us.data <- data[data$DRG.Definition == input$drg,]
         
+        par(mfrow=c(1,3))
+        boxplot(state1.data$Average.Covered.Charges,
+                xlab = as.character(input$state1),
+                ylab = 'Charge Amount',
+                main = 'Average Charges for this DRG')
+        boxplot(us.data$Average.Covered.Charges,
+                xlab = 'US',
+                ylab = 'Charge Amount',
+                main = 'National Average Charges for this DRG')
+        boxplot(state2.data$Average.Covered.Charges,
+                xlab = as.character(input$state2),
+                ylab = 'Charge Amount',
+                main = 'Average Charges for this DRG') 
     })
+    
+    output$box_plot <- renderPlot({
+        
+        state1.data <- data[data$DRG.Definition == input$drg & data$Provider.State == input$state1,]
+        state2.data <- data[data$DRG.Definition == input$drg & data$Provider.State == input$state2,]
+        us.data <- data[data$DRG.Definition == input$drg,]
+        if (as.character(input$metric) == 'Charges'){
+            metric <- 'Average.Covered.Charges'
+        }
+        else if (as.character(input$metric) == 'Payments'){
+            metric <- 'Average.Total.Payments'
+        }
+        else {
+            metric <- 'Average.Medicare.Payments'
+        }
+        
+        par(mfrow=c(1,3))
+        boxplot(state1.data[metric],
+                xlab = as.character(input$state1),
+                ylab = 'Charge Amount',
+                main = paste0('Average ', as.character(input$metric),
+                              ' for\n', as.character(input$drg)))
+        boxplot(us.data[metric],
+                xlab = 'US',
+                ylab = 'Charge Amount',
+                main = paste0('National Average ', as.character(input$metric), 
+                              ' for\n', as.character(input$drg)))
+        boxplot(state2.data[metric],
+                xlab = as.character(input$state2),
+                ylab = 'Charge Amount',
+                main = paste0('Average ', as.character(input$metric),
+                              ' for\n', as.character(input$drg))) 
+    })
+    
 })
--- ui.R
+++ ui.R
@@ -3,20 +3,25 @@ data <- read.csv(unz('./data/IPPS_FY2011.csv.zip', 'IPPS_FY2011.csv'))
 
 shinyUI(bootstrapPage(
     
-    selectInput(inputId = 'state',
+    selectInput(inputId = 'state1',
                 label = 'State',
-                choices = c('United States (All)', levels(data$Provider.State)),
-                selected = 'United States (All)'),
+                choices = c(levels(data$Provider.State)),
+                selected = 'MD'),
+    
+    selectInput(inputId = 'state2',
+                label = 'State',
+                choices = c(levels(data$Provider.State)),
+                selected = 'PA'),
     
     selectInput(inputId = 'drg',
                 label = 'Diagnosis-related Group',
                 choices = c(levels(data$DRG.Definition)),
                 selected = '039 - EXTRACRANIAL PROCEDURES W/O CC/MCC'),
     
-    radioButtons(inputId = 'chart',
-                 label = 'Chart Type',
-                 choices = c('Histogram', 'Boxplot'),
-                 selected = 'Histogram'),
+    radioButtons(inputId = 'metric',
+                 label = "Metric",
+                 choices = c('Charges', 'Payments', 'Medicare Payments'),
+                 selected = 'Charges'),
     
-    plotOutput(outputId = 'hist_charge_plot', height = '300px')    
+    plotOutput(outputId = 'box_plot', height = '300px')
 ))
